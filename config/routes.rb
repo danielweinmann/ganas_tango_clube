5cb7bd933f6119f29a97d56f739a138034325285
Daniel Weinmann <danielweinmann@gmail.com>
2015-06-25 17:17:17 -0300

    Changes routes to pretty URL

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
 
   root "leads#moinhos"
   resources :subscribers, only: [:create]
-  resources :nightlife, only: [] do
-    collection do
-      get :male
-    end
-  end
+
+  get "Entenda porque você sente um vazio depois de sair na noite homens".parameterize => "nightlife#male", as: :nightlife_male
 
 end
