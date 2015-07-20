df68e6ce25a0d88b8467084d40eda4147b77eeb5
Daniel Weinmann <danielweinmann@gmail.com>
2015-07-20 13:23:02 -0300

    More text review

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Leads
   get "Você sabia que o tango é uma dança social de improviso".parameterize => "leads#social", as: :leads_social
-  get "4 dicas simples para você começar a dançar tango".parameterize => "leads#how_to_start", as: :leads_how_to_start
+  get "4 dicas para você começar a dançar tango".parameterize => "leads#how_to_start", as: :leads_how_to_start
   get "Próximos eventos".parameterize => "leads#next_events", as: :leads_next_events
 
   # Offers
